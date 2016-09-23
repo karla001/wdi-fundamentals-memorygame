081f1d919c417b049e0f1ecf694f1a138106d113
karla <karla@karlas-MacBook-Pro.local>
2016-09-22 22:58:58 -0700

    created if else statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,8 @@ var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
+ if (cardTwo == cardFour) {
+   alert( "You found a match!");
+ }else {
+   alert("Sorry, try again.");
+ }
